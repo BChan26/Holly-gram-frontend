a462f28d7de02761cfd2b7e049d4b54c7a40b57a
Brandon Chan <bchan26@bu.edu>
2022-12-01 11:41:13 -0500

    front end done pre-auth

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,19 +1,68 @@
 import Nav from './Nav'
+import Hollygram from '../assets/Hollygram.png'
+import RegisterPageIcon from '../assets/RegisterPageIcon.png'
 
 export default function Feed () {
     return (
+<div>
+
+    <div id="NavBarLocation">
+    <Nav/>
+    </div>
 
     <div id='FeedContent'>
 
-    <Nav/>
-
         <div className="FeedPosts">
-        <h1>Hello World!</h1>
+        <h1>HollyGram Mapped Feed</h1>
+        
+                <div className="IndividualFeedPosts">
+                    {/* Will be mapped through with a function/query */}
+                    
+                    <div className="TopOfPost">
+                        <img src={Hollygram}/>
+                        <h3>UserName</h3>
+                    </div>
+
+                    <div className="PictureOfPost">
+                        <img src={RegisterPageIcon}/>
+                    </div>
+
+                    <div className="LikesCommentsOfPost">
+                        <h4>Like</h4>
+                        <h4>Comment</h4>
+                        <h4>Tags</h4>
+                    </div>
+                    
+                </div>
+
+                <br/>
+
+                <div className="IndividualFeedPosts">
+                    {/* Will be mapped through with a function/query */}
+                    
+                    <div className="TopOfPost">
+                        <img src={Hollygram}/>
+                        <h3>UserName</h3>
+                    </div>
+
+                    <div className="PictureOfPost">
+                        <img src={RegisterPageIcon}/>
+                    </div>
+
+                    <div className="LikesCommentsOfPost">
+                        <h4>Like</h4>
+                        <h4>Comment</h4>
+                        <h4>Tags</h4>
+                    </div>
+                    
+                </div>
+
         </div>
 
-        <div className="FeedPosts">
-        <h1>We'll set this up so it maps out content</h1>
+        <div className="SuggestionsForYou">
+        <h1>Suggestions For You</h1>
         </div>
 
     </div>
+</div>
     )}
