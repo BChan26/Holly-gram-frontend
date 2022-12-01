0002d3def0f56130239c1f70104a8a36932179d4
Brandon Chan <bchan26@bu.edu>
2022-12-01 09:24:29 -0500

    Most up to date front end with a temporary home landing page to route to each component for feed+nav, signin, register

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,9 +1,30 @@
 export default function SignIn () {
     return (
-    <div id='SignInContent'>
+        <div id='SignInContent'>
 
-        <div id="">
-        SignIn Content
+        <div id="SignInInfo">
+
+            <div id="SignInInfoLogo">
+            <h1>❄️Holly-Gram❄️</h1>
+            </div>
+
+            <div id="SignInInfoInput">
+                <form id="SignInInfoForm">
+                                        
+                    <label>
+                    Username
+                    <input type="text" name="Username" />
+                    </label>
+                                        
+                    <label>
+                    Password
+                    <input type="text" name="Password" />
+                    </label>
+
+                    <input type="submit" value="Sign In" />
+                </form>
+            </div>
+
         </div>
 
 
